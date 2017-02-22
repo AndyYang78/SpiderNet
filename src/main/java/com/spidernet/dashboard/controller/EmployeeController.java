09ddf2ab89d27ebfb6efd3b6f372374205a816a8
AndyYang78 <965998042@qq.com>
2017-02-23 01:46:23 +0800

    EmployeeController

---
+++ src/main/java/com/spidernet/dashboard/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.spidernet.dashboard.controller;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spidernet.dashboard.service.EmployeeService;
+
+
+@RestController
+@RequestMapping(value="/employee")
+public class EmployeeController {     
+	@Resource 
+	EmployeeService userService;
+	
+	@RequestMapping("/loginPage")
+	public String loginPage(final HttpServletRequest request, final HttpServletResponse response) {
+		return "login";
+	}
+	@RequestMapping("/checkUser")
+	public String checkUser(final HttpServletRequest request, final HttpServletResponse response) {
+		return "index";
+	}
+	
+	
+}
